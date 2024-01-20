b65cbf2bba3dc6a26ed1b948ba8d5cf58df6ed4e
Jan Deinhard <jan.deinhard@gmail.com>
2024-01-20 09:49:46 +0100

    refactor: remove commented code

--- src/vm/EvaVM.cpp
+++ src/vm/EvaVM.cpp
@@ -11,42 +11,9 @@ EvaVM::EvaVM()
 
 EvaValue EvaVM::exec([[maybe_unused]] const std::string& program) {
   const auto ast = parser->parse(program);
-
   co = compiler->compile(ast);
-
-  // constants.push_back(AllocString("Hello, "));
-  // constants.push_back(AllocString("World!"));
-
-  // code = {to_uint8(OpCode::OP_CONST),  0,
-  //         to_uint8(OpCode::OP_CONST),  1,
-  //         to_uint8(OpCode::OP_CONCAT), to_uint8(OpCode::OP_HALT)};
-
-  // constants.push_back(Number(120));
-  // constants.push_back(Number(100));
-  // constants.push_back(Number(10));
-  // constants.push_back(Number(2));
-  // constants.push_back(Number(10));
-
-  // code = {to_uint8(OpCode::OP_CONST),
-  //         0,
-  //         to_uint8(OpCode::OP_CONST),
-  //         1,
-  //         to_uint8(OpCode::OP_ADD),
-  //         to_uint8(OpCode::OP_CONST),
-  //         2,
-  //         to_uint8(OpCode::OP_SUB),
-  //         to_uint8(OpCode::OP_CONST),
-  //         3,
-  //         to_uint8(OpCode::OP_MUL),
-  //         to_uint8(OpCode::OP_CONST),
-  //         4,
-  //         to_uint8(OpCode::OP_DIV),
-  //         to_uint8(OpCode::OP_HALT)};
-
   ip = &co->code[0];
-
   sp = &stack[0];
-
   return eval();
 }
 
