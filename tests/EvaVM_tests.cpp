a97f2b20e27ec4b792100be2557b7ba082cf18af
Jan Deinhard <jan.deinhard@gmail.com>
2024-01-20 14:45:10 +0100

    feat: more unit tests

--- tests/EvaVM_tests.cpp
+++ tests/EvaVM_tests.cpp
@@ -14,6 +14,24 @@ TEST_CASE("Add number expression", "[exec]") {
   REQUIRE(asNumber(result) == 42);
 }
 
+TEST_CASE("Subtract number expression", "[exec]") {
+  EvaVM vm;
+  const auto result = vm.exec(R"( (- 20 22) )");
+  REQUIRE(asNumber(result) == -2);
+}
+
+TEST_CASE("Multiply number expression", "[exec]") {
+  EvaVM vm;
+  const auto result = vm.exec(R"( (* 20 22) )");
+  REQUIRE(asNumber(result) == 440);
+}
+
+TEST_CASE("Divide number expression", "[exec]") {
+  EvaVM vm;
+  const auto result = vm.exec(R"( (/ 20 4) )");
+  REQUIRE(asNumber(result) == 5);
+}
+
 TEST_CASE("String expression", "[exec]") {
   EvaVM vm;
   const auto result = vm.exec(R"("hello")");
@@ -36,4 +54,70 @@ TEST_CASE("Boolean expression - false", "[exec]") {
   EvaVM vm;
   const auto result = vm.exec(R"(false)");
   REQUIRE(asBoolean(result) == false);
+}
+
+TEST_CASE("Less than expression", "[exec]") {
+  EvaVM vm;
+
+  const auto result1 = vm.exec(R"( (< 20 4) )");
+  REQUIRE(asBoolean(result1) == false);
+
+  const auto result2 = vm.exec(R"( (< 4 20) )");
+  REQUIRE(asBoolean(result2) == true);
+}
+
+TEST_CASE("Greater than expression", "[exec]") {
+  EvaVM vm;
+
+  const auto result1 = vm.exec(R"( (> 20 4) )");
+  REQUIRE(asBoolean(result1) == true);
+
+  const auto result2 = vm.exec(R"( (> 4 20) )");
+  REQUIRE(asBoolean(result2) == false);
+}
+
+TEST_CASE("Equality expression", "[exec]") {
+  EvaVM vm;
+
+  const auto result1 = vm.exec(R"( (== 20 4) )");
+  REQUIRE(asBoolean(result1) == false);
+
+  const auto result2 = vm.exec(R"( (== 4 4) )");
+  REQUIRE(asBoolean(result2) == true);
+}
+
+TEST_CASE("Greater than or equal expression", "[exec]") {
+  EvaVM vm;
+
+  const auto result1 = vm.exec(R"( (>= 20 4) )");
+  REQUIRE(asBoolean(result1) == true);
+
+  const auto result2 = vm.exec(R"( (>= 4 20) )");
+  REQUIRE(asBoolean(result2) == false);
+
+  const auto result3 = vm.exec(R"( (>= 20 20) )");
+  REQUIRE(asBoolean(result3) == true);
+}
+
+TEST_CASE("Less than or equal expression", "[exec]") {
+  EvaVM vm;
+
+  const auto result1 = vm.exec(R"( (<= 20 4) )");
+  REQUIRE(asBoolean(result1) == false);
+
+  const auto result2 = vm.exec(R"( (<= 4 20) )");
+  REQUIRE(asBoolean(result2) == true);
+
+  const auto result3 = vm.exec(R"( (<= 4 4) )");
+  REQUIRE(asBoolean(result3) == true);
+}
+
+TEST_CASE("Inequality expression", "[exec]") {
+  EvaVM vm;
+
+  const auto result1 = vm.exec(R"( (!= 20 4) )");
+  REQUIRE(asBoolean(result1) == true);
+
+  const auto result2 = vm.exec(R"( (!= 4 4) )");
+  REQUIRE(asBoolean(result2) == false);
 }
